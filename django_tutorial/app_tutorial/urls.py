fda9971a6e9a8efe5b61db6d82396e2106401100
Igor <igorkatkevich.work@gmail.com>
2022-03-14 22:55:21 +0300

    review_list and book_review_list

--- django_tutorial/app_tutorial/urls.py
+++ django_tutorial/app_tutorial/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name= 'index'),
-    path('about/', views.about),
-    path('home/', views.home),
-    path('book_detail/<int:book_id>/', views.book_detail, name='book_detail')
+    path('about/', views.about, name= 'about'),
+    path('home/', views.home, name= 'home'),
+    path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('review_detail/<int:review_id>/', views.review_detail, name='review_detail'),
+    path('book_list/', views.book_list, name='book_list'),
+    path('review_list/', views.review_list, name='review_list'),
+    path('book_review_list/<int:book_id>/', views.book_review_list, name='book_review_list'),
 ]
--- django_tutorial/app_tutorial/views.py
+++ django_tutorial/app_tutorial/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Book
+from .models import Book, Review
 
 
 def index(request):
@@ -17,3 +17,22 @@ def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'book_detail.html', {'book': book})
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+
+def review_detail(request, review_id):
+    review = Review.objects.get(id=review_id)
+    return render(request, 'review_detail.html', {'review': review})
+
+
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, 'review_list.html', {'reviews': reviews})
+
+
+def book_review_list(request, book_id):
+    book = Book.objects.get(id=book_id)
+    reviews = Review.objects.filter(book=book_id)
+    return render(request, 'review_list.html', {'reviews': reviews})
